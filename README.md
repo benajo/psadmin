16df14ed0aead22dd1a9cafd180d7e26dd5dc3a8
Ben Jovanic <benjovanic6@gmail.com>
2018-05-23 22:43:54 +0100

    Environment setup complete

---
+++ README.md
@@ -0,0 +1,20 @@
+# Pluralsight Administration
+
+This app demos the Pluralsight course [Building Applications with React and Flux](https://app.pluralsight.com/library/courses/react-flux-building-applications)
+
+# Install
+
+Run the following commands:
+
+1. `npm install`
+2. `npm install -g gulp@3.9.0`
+
+# Running
+
+Run the command:
+- `gulp`
+
+
+Then open:
+
+- http://localhost:9005
---
+++ gulpfile.js
@@ -0,0 +1,75 @@
+"use strict";
+
+var gulp = require('gulp');
+var connect = require('gulp-connect'); // runs local dev server
+var open = require('gulp-open'); // open a url in a web browser
+var browserify = require('browserify'); // bundles js
+var reactify = require('reactify'); // transforms react jsx to js
+var source = require('vinyl-source-stream');  // use conventional text streams with gulp
+var concat = require('gulp-concat'); // concats files
+var eslint = require('gulp-eslint'); // lint js and jsx files
+
+var config = {
+    port: 9005,
+    devBaseUrl: 'http://localhost',
+    paths: {
+        html: './src/*.html',
+        js: './src/**/*.js',
+        css: [
+            './node_modules/bootstrap/dist/css/bootstrap.min.css',
+            './node_modules/bootstrap/dist/css/bootstrap-theme.min.css',
+        ],
+        dist: './dist',
+        mainJs: './src/main.js'
+    }
+}
+
+// start a local dev server
+gulp.task('connect', () => {
+    connect.server({
+        root: ['dist'],
+        port: config.port,
+        base: config.devBaseUrl,
+        livereload: true
+    });
+});
+
+gulp.task('open', ['connect'], () => {
+    gulp.src('./dist/index.html')
+        .pipe(open({ url: `${config.devBaseUrl}+${config.port}/` }));
+});
+
+gulp.task('html', () => {
+    gulp.src(config.paths.html)
+        .pipe(gulp.dest(config.paths.dist))
+        .pipe(connect.reload());
+});
+
+gulp.task('js', () => {
+    browserify(config.paths.mainJs)
+        .transform(reactify)
+        .bundle()
+        .on('error', console.error.bind(console))
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest(`${config.paths.dist}/scripts`))
+        .pipe(connect.reload());
+});
+
+gulp.task('css', () => {
+    gulp.src(config.paths.css)
+        .pipe(concat('bundle.css'))
+        .pipe(gulp.dest(`${config.paths.dist}/css`));
+});
+
+gulp.task('lint', () => {
+    return gulp.src(config.paths.js)
+        .pipe(eslint({ config: 'eslint.config.json' }))
+        .pipe(eslint.format());
+});
+
+gulp.task('watch', () => {
+    gulp.watch(config.paths.html, ['html']);
+    gulp.watch(config.paths.js, ['js', 'lint']);
+});
+
+gulp.task('default', ['html', 'js', 'css', 'lint', 'open', 'watch']);
